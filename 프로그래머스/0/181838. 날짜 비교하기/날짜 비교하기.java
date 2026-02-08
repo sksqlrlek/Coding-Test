954d29ba40f12623f11c47663f16c61a1c6b2328
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-02-09 08:59:48 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        int a = (date1[0] * 10000) + (date1[1] * 100) + (date1[2] * 1);
+        int b = (date2[0] * 10000) + (date2[1] * 100) + (date2[2] * 1);
+        return a < b ? 1 : 0;
+    }
+}

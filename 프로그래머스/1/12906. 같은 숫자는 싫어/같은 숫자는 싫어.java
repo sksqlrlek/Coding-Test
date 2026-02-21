ae1f0cf391bd15fa61f640efa853f5e4a7406799
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-02-21 10:43:48 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.38 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i - 1] != arr[i]) {
+                list.add(arr[i]);
+            }
+        }
+        
+        return list.stream().mapToInt(x -> x).toArray();
+    }
+}

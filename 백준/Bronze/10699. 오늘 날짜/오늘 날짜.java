3af894121c17815c607aaedbd5110a0e1f77a8b2
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-03-18 20:48:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 172 ms, Memory: 16852 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.time.LocalDate;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(LocalDate.now());
+    }
+}

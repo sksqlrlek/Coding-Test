9389b50134abb3d77a10063a5b7c88d9148436ff
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-03-27 07:17:25 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(gcd(A, B));
+        System.out.println(lcm(A, B));
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+}

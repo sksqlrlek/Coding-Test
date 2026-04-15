fd55531a29b15bf84b14512d8138b8d5eed2292f
Tamoon <81087538+sksqlrlek@users.noreply.github.com>
2026-04-16 07:37:23 +0900

    [level 2] Title: H-Index, Time: 0.61 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i = 0; i < n; i++){
+            if(citations[i] >= n - i) return n - i;
+        }
+        return 0;
+    }
+}
